65a6e841da942c741221f5bf2cb575ea38d18763
Guillaume Binet <gbin@gootz.net>
2025-02-07 14:38:04 -0600

    temp

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,55 +1,25 @@
 use cu29::prelude::*;
 use bincode::{Decode, Encode};
+use cu_gstreamer::CuGstBuffer;
 
-// Define a message type
 #[derive(Default, Debug, Clone, Encode, Decode)]
 pub struct MyPayload {
     value: i32,
 }
 
-// Defines a source (ie. driver)
-#[derive(Default)]
-pub struct MySource {}
 
-// Needs to be fully implemented if you want to have a stateful task.
-impl Freezable for MySource {}
+pub struct MyTask {}
 
-impl<'cl> CuSrcTask<'cl> for MySource {
-    type Output = output_msg!('cl, MyPayload);
-
-    fn new(_config: Option<&ComponentConfig>) -> CuResult<Self>
-    where
-        Self: Sized,
-    {
-        Ok(Self {})
-    }
-
-    // don't forget the other lifecycle methods if you need them: start, stop, preprocess, postprocess
-
-    fn process(&mut self, _clock: &RobotClock, output: Self::Output) -> CuResult<()> {
-        // Generated a 42 message.
-        output.set_payload(MyPayload { value: 42 });
-        Ok(())
-    }
-}
-
-// Defines a processing task
-pub struct MyTask {
-    // if you add some task state here, you need to implement the Freezable trait
-}
-
-// Needs to be fully implemented if you want to have a stateful task.
 impl Freezable for MyTask {}
 
 impl<'cl> CuTask<'cl> for MyTask {
-    type Input = input_msg!('cl, MyPayload);
+    type Input = input_msg!('cl, CuGstBuffer);
     type Output = output_msg!('cl, MyPayload);
 
     fn new(_config: Option<&ComponentConfig>) -> CuResult<Self>
     where
         Self: Sized,
     {
-        // add the task state initialization here
         Ok(Self {})
     }
 
@@ -61,8 +31,8 @@ impl<'cl> CuTask<'cl> for MyTask {
         input: Self::Input,
         output: Self::Output,
     ) -> CuResult<()> {
-        debug!("Received message: {}", input.payload().unwrap().value);
-        output.set_payload(MyPayload { value: 43 });
+        println!("Received Gstreamer: {:?}", input.payload().unwrap());
+        output.set_payload(MyPayload { value: 42 });
         Ok(()) // outputs another message for downstream
     }
 }
