4c03787c6c1f4b4c18801a8ba5ed1097d8d24b14
Guillaume Binet <gbin@gootz.net>
2025-02-11 15:10:44 -0600

    added rerun

--- src/tasks.rs
+++ src/tasks.rs
@@ -3,14 +3,16 @@ use bincode::{Decode, Encode};
 use cu_gstreamer::CuGstBuffer;
 use std::thread;
 use std::time::Duration;
-
+use rerun::{ChannelDatatype, ColorModel, Image, RecordingStream, RecordingStreamBuilder};
 
 #[derive(Default, Debug, Clone, Encode, Decode)]
 pub struct MyPayload {
     value: i32,
 }
 
-pub struct MyTask {}
+pub struct MyTask {
+    rec: RecordingStream,
+}
 
 impl Freezable for MyTask {}
 
@@ -22,7 +24,10 @@ impl<'cl> CuTask<'cl> for MyTask {
     where
         Self: Sized,
     {
-        Ok(Self {})
+        let rec = RecordingStreamBuilder::new("Camera B&W Viz")
+            .connect_tcp_opts(std::net::SocketAddr::from(([192, 168, 1, 181], 9876)), None)
+            .unwrap();
+        Ok(Self {rec})
     }
 
     fn process(
@@ -36,7 +41,22 @@ impl<'cl> CuTask<'cl> for MyTask {
             thread::sleep(Duration::from_millis(100)); 
             return Ok(());
         }
-        println!("Received Gstreamer: {:?}", payload.unwrap());
+
+        let data = input.payload().unwrap().map_readable().unwrap();
+        debug!("Received Gstreamer: {}", data.len());
+
+        let width = 1920;
+        let height = 1080;
+        let y_plane_size = width * height;
+        let grey_image = &data[0..y_plane_size];
+
+        let image = Image::from_color_model_and_bytes(
+            grey_image.to_vec(),
+            [width as u32, height as u32],
+            ColorModel::L,
+            ChannelDatatype::U8,
+        );
+        self.rec.log("camera/image", &image).unwrap();
         output.set_payload(MyPayload { value: 42 });
         Ok(())
     }
