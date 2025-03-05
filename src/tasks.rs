59074ecf592a9a459ac2e1b34e117437a1db45a6
Guillaume Binet <gbin@gootz.net>
2025-03-04 18:37:09 -0600

    full loop on MSP

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,21 +1,16 @@
 use cu29::prelude::*;
-use bincode::{Decode, Encode};
+use cu_msp_lib::structs::MspRequest;
+use cu_msp_sink::MspRequestBatch;
+use cu_msp_src::MspResponseBatch;
 
-// Define a message type
-#[derive(Default, Debug, Clone, Encode, Decode)]
-pub struct MyPayload {
-    value: i32,
+pub struct DroneControl {
 }
 
-// Defines a source (ie. driver)
-#[derive(Default)]
-pub struct MySource {}
+impl Freezable for DroneControl {}
 
-// Needs to be fully implemented if you want to have a stateful task.
-impl Freezable for MySource {}
-
-impl<'cl> CuSrcTask<'cl> for MySource {
-    type Output = output_msg!('cl, MyPayload);
+impl<'cl> CuTask<'cl> for DroneControl {
+    type Input = input_msg!('cl, MspResponseBatch);
+    type Output = output_msg!('cl, MspRequestBatch);
 
     fn new(_config: Option<&ComponentConfig>) -> CuResult<Self>
     where
@@ -23,37 +18,6 @@ impl<'cl> CuSrcTask<'cl> for MySource {
     {
         Ok(Self {})
     }
-
-    // don't forget the other lifecycle methods if you need them: start, stop, preprocess, postprocess
-
-    fn process(&mut self, _clock: &RobotClock, output: Self::Output) -> CuResult<()> {
-        // Generated a 42 message.
-        output.set_payload(MyPayload { value: 42 });
-        Ok(())
-    }
-}
-
-// Defines a processing task
-pub struct MyTask {
-    // if you add some task state here, you need to implement the Freezable trait
-}
-
-// Needs to be fully implemented if you want to have a stateful task.
-impl Freezable for MyTask {}
-
-impl<'cl> CuTask<'cl> for MyTask {
-    type Input = input_msg!('cl, MyPayload);
-    type Output = output_msg!('cl, MyPayload);
-
-    fn new(_config: Option<&ComponentConfig>) -> CuResult<Self>
-    where
-        Self: Sized,
-    {
-        // add the task state initialization here
-        Ok(Self {})
-    }
-
-    // don't forget the other lifecycle methods if you need them: start, stop, preprocess, postprocess
 
     fn process(
         &mut self,
@@ -61,32 +25,13 @@ impl<'cl> CuTask<'cl> for MyTask {
         input: Self::Input,
         output: Self::Output,
     ) -> CuResult<()> {
-        debug!("Received message: {}", input.payload().unwrap().value);
-        output.set_payload(MyPayload { value: 43 });
+        if let Some(batch) = input.payload() {
+            debug!("Received response batch: {}", batch);
+        }
+        let mut batch = MspRequestBatch::new();
+        batch.push(MspRequest::MspBatteryState);
+        output.set_payload(batch);
         Ok(()) // outputs another message for downstream
     }
 }
 
-// Defines a sink (ie. actualtion)
-#[derive(Default)]
-pub struct MySink {}
-
-// Needs to be fully implemented if you want to have a stateful task.
-impl Freezable for MySink {}
-
-impl<'cl> CuSinkTask<'cl> for MySink {
-    type Input = input_msg!('cl, MyPayload);
-
-    fn new(_config: Option<&ComponentConfig>) -> CuResult<Self>
-    where
-        Self: Sized,
-    {
-        Ok(Self {})
-    }
-    // don't forget the other lifecycle methods if you need them: start, stop, preprocess, postprocess
-
-    fn process(&mut self, _clock: &RobotClock, input: Self::Input) -> CuResult<()> {
-        debug!("Sink Received message: {}", input.payload().unwrap().value);
-        Ok(())
-    }
-}
