b4f209bca1fa735b3b942332b2f61efd68a7f3fa
Guillaume Binet <gbin@gootz.net>
2025-03-05 16:17:04 -0600

    pass on integration

--- d
+++ d
@@ -1,4 +1,5 @@
 #/bin/bash
+set -E
 cargo build --target aarch64-unknown-linux-musl
 scp target/aarch64-unknown-linux-musl/debug/drone copper8:
 
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,4 +1,5 @@
 use cu29::prelude::*;
+use cu_apriltag::AprilTagDetections;
 use cu_msp_lib::structs::MspRequest;
 use cu_msp_sink::MspRequestBatch;
 use cu_msp_src::MspResponseBatch;
@@ -9,7 +10,7 @@ pub struct DroneControl {
 impl Freezable for DroneControl {}
 
 impl<'cl> CuTask<'cl> for DroneControl {
-    type Input = input_msg!('cl, MspResponseBatch);
+    type Input = input_msg!('cl, MspResponseBatch, AprilTagDetections);
     type Output = output_msg!('cl, MspRequestBatch);
 
     fn new(_config: Option<&ComponentConfig>) -> CuResult<Self>
@@ -25,8 +26,12 @@ impl<'cl> CuTask<'cl> for DroneControl {
         input: Self::Input,
         output: Self::Output,
     ) -> CuResult<()> {
-        if let Some(batch) = input.payload() {
+        let (maybe_batch, maybe_detections) = input;
+        if let Some(batch) = maybe_batch.payload() {
             debug!("Received response batch: {}", batch);
+        }
+        if let Some(detections) = maybe_detections.payload() {
+            debug!("Received detections: {}", detections);
         }
         let mut batch = MspRequestBatch::new();
         batch.push(MspRequest::MspBatteryState);
